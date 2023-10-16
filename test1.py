86e904239ea39a0f0dd92a3bed6884e02fd18c80
KoudaiNakagawa <22111247kn@tama.ac.jp>
2023-10-16 11:46:17 +0900

    AddTestCode

---
+++ test1.py
@@ -0,0 +1,6 @@
+#print((l1:=list(map(int,input().split())))[0]+l1[1])
+#print((l1:=list(map(int,input().split())))[0]-l1[1])
+#print((l1:=list(map(int,input().split())))[0]*l1[1])
+#print((l1:=list(map(int,input().split())))[0]/l1[1])
+print((l1:=list(map(int,input().split())))[0]**l1[1])
+
---
+++ ts.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+public class test1
+{
+    public static void Main() {
+
+        string path = @"./test1.py";
+        var textList = ReadFile(path);
+
+        UsePythonFunctions(textList);
+    }
+
+    static List<string> ReadFile(string path) {
+        var textList = new List<string>(File.ReadAllLines(path));
+        return textList;
+    }
+
+    static void UsePythonFunctions(List<string> textList) {
+        
+        foreach( string text in textList) {
+            MatchCollection results = Regex.Matches(text,  @"[a-zA-Z][a-zA-Z0-9]*|[0-9]+|==|\*\*|:=|::|[=+*-/%()\[\]{}<>'.,:#\\s\\t\\]" );
+        
+            foreach (Match m in results) {
+                string s = m.Value;
+            
+                Console.Write(s);
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+    // ~~~PythonLikeFunctions~~~
+
+    static string input(string s="", string end="\n") {
+        if (s != ""){
+            print(s, end);
+        }
+        return Console.ReadLine();
+    }
+
+    static void print(dynamic d, string end="\n") { 
+        Console.Write(d+end);
+    }
+
+    static int intP(dynamic d) {
+        return int.Parse(d.ToString());
+    }
+}
